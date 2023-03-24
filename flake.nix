e82dd53924b12c3b4cb1a00696a9cbefded43dc9
Michael Litchard <litchard.michael@gmail.com>
2023-03-23 17:01:20 -0700

    first

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
         };
 
         # Configurations for my (only) macOS machine (using nix-darwin)
-        darwinConfigurations = {
+        darwinConfigurations.eudaimonia = {
           appreciate = self.nixos-flake.lib.mkARMMacosSystem {
             imports = [
               self.darwinModules.default # Defined in nix-darwin/default.nix
